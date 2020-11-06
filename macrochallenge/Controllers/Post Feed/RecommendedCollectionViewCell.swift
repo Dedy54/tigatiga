1b451416ac08460702ca5afb8f270a27b833c796
Glendito <glendito.palendeng@gmail.com>
2020-11-06 15:32:20 +0700

    commit feed page UI 6/10

---
+++ macrochallenge/Controllers/Post Feed/RecommendedCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  RecommendedCollectionViewCell.swift
+//  macrochallenge
+//
+//  Created by Glendito Jeremiah Palendeng on 05/11/20.
+//  Copyright © 2020 tigatiga. All rights reserved.
+//
+
+import UIKit
+
+class RecommendedCollectionViewCell: UICollectionViewCell {
+    
+    
+    
+    @IBOutlet weak var recommendedImage: UIImageView!
+    
+    @IBOutlet weak var recommendedView: UIView!
+    
+}
