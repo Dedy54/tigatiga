8d8529fbb92b424ccbed416533795dc7ff76aa88
Dedy Yuristiawan <dedyy54@gmail.com>
2020-11-17 09:51:58 +0700

    add chat detail vc

---
+++ macrochallenge/Controllers/ChatDetail/ChatDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatDetailViewController.swift
+//  macrochallenge
+//
+//  Created by Dedy Yuristiawan on 17/11/20.
+//  Copyright © 2020 tigatiga. All rights reserved.
+//
+
+import UIKit
+
+class ChatDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
