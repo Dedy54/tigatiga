00b1c24351ac8b7e67b0ba1eb5caa268f1acecdd
Dedy Yuristiawan <dedyy54@gmail.com>
2020-11-17 09:42:32 +0700

    add complete profile chat list and chat detail

---
+++ macrochallenge/Controllers/Chat/Cell/ChatCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ChatCollectionViewCell.swift
+//  macrochallenge
+//
+//  Created by Dedy Yuristiawan on 16/11/20.
+//  Copyright © 2020 tigatiga. All rights reserved.
+//
+
+import UIKit
+
+class ChatCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ macrochallenge/Controllers/Chat/ChatViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ChatViewController.swift
+//  macrochallenge
+//
+//  Created by Dedy Yuristiawan on 16/11/20.
+//  Copyright © 2020 tigatiga. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
